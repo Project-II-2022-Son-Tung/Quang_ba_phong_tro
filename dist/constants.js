ab6842d097ec33476ede588923f85d6b68412db0
@sonnvc <sonnvc@a2ztech.vn>
2022-07-03 01:21:50 +0700

    remove dist folder

--- dist/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.__prod__ = exports.COOKIE_NAME = void 0;
-require('dotenv').config();
-exports.COOKIE_NAME = 'beematie-cookie';
-exports.__prod__ = (process.env.NODE_ENV === 'production');
-//# sourceMappingURL=constants.js.map
